24adfe6522817e9a3f1a1bd859739da5c868ed36
Vladyslav Pobihailo <vladyslav.pobihailo@boosta.co>
2024-03-03 18:35:29 +0200

    add SignMessage component

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex items-center space-x-2 bg-white p-2 rounded-lg shadow-md mb-4 justify-center md:justify-end">
+    <SignMessage/>
     <ConnectWallet/>
 </div>
 </template>
 
 <script setup lang="ts">
 import ConnectWallet from '@/components/ConnectWallet.vue';
+import SignMessage from '@/components/SignMessage.vue';
 
 </script>
---
+++ src/components/SignMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <Button v-if="accountAddress" @click="signMessage" class="flex gap-1 items-center">
+    <span>
+      {{ signButtonText }}
+    </span>
+</Button>
+</template>
+
+<script setup lang="ts">
+  import Button from './ui/button/Button.vue';
+  import Web3 from 'web3'
+  import { computed, ref } from 'vue';
+  import { useWalletStore } from '@/store/wallet.store';
+  import { sliceTransaction } from '@/composable/sliceTransaction';
+  import { createToast } from '@/composable/createToast'
+
+  const ethereum = (window as any).ethereum;
+  const web3 = new Web3(ethereum);
+  const wallet = useWalletStore();
+  let accountAddress = computed(() => wallet.getAccountAddress);
+  let signButtonText = ref('Sign Message');
+
+  const signMessage = async function(){
+    try{
+      const signatureData = await web3.eth.personal.sign(web3.utils.utf8ToHex("test"), accountAddress.value, "")
+      signButtonText.value = sliceTransaction(signatureData)
+      console.log(signatureData)
+      createToast('MetaMask Personal Message Signature', 'User signed message signature', 'success')
+    } catch(error){
+      createToast('MetaMask Personal Message Signature', 'User denied message signature', 'error')
+    }
+  }
+
+</script>
